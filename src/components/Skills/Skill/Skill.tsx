3e02b975494ee367b2aefad02ec91b7e01e48085
katrina <katrina.nov13@gmail.com>
2022-04-27 21:50:47 +0300

    responsive structure hero, skills

--- src/components/Skills/Skill/Skill.tsx
+++ src/components/Skills/Skill/Skill.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import style from './Skill.module.css';
 
-const Skill = () => {
+type PropsType = {
+    title: string
+    description: string
+}
+
+const Skill = (props: PropsType) => {
     return (
-        <div>
-            
+        <div className={style.skill}>
+            <div className={style.icon}></div>
+            <h3>{props.title}</h3>
+            <div className={style.description}>{props.description}</div>
         </div>
     );
 };
--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -9,7 +9,12 @@ const Skills = () => {
             <div className={styleContainer.container}>
                 <h2>My skills</h2>
                 <div className={style.skills}>
-                    <Skill/>
+                    <Skill
+                        title={'React'}
+                        description={'Lorem Ipsum is simply dummy text of the printing and typesetting industry.  since the 1500s, when an unknown '}
+                    />
+                    <Skill title={'Js'} description={'Lorem Ipsum has been the industry\'s standard dummy text ever'}/>
+                    <Skill title={'CSS'} description={'printer took a galley of type and scrambled it to make a type specimen book.'}/>
                 </div>
             </div>
         </div>
